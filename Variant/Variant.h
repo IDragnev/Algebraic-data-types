2445be0e6e7d61f40e511abacd91e38371f4b5ea
IDragnev <root@DESKTOP-14OVU4I.localdomain>
2019-08-11 14:52:50 +0300

    Add the interface of Variant

---
+++ Variant/Variant.h
@@ -0,0 +1,69 @@
+
+#include "VariantChoice.h"
+#include "VariantStorage.h"
+
+namespace IDragnev
+{
+    template <typename... Types>
+    class Variant
+        : private Detail::VariantStorage<Types...>,
+          private Detail::VariantChoice<Types, Types...>...
+    {
+    private:
+        template <typename T, typename... Types>
+        friend class VariantChoice;
+
+    public:
+        using VariantChoice<Types, Types...>::VariantChoice...;
+
+        Variant();                                         
+        Variant(Variant const& source);                    
+        Variant(Variant&& source);                   
+        ~Variant() { destroy(); }
+        
+        template <typename... SourceTypes>
+        Variant(const Variant<SourceTypes...>& source);
+        
+        template<typename... SourceTypes>
+        Variant(Variant<SourceTypes...>&& source);
+
+        using VariantChoice<Types, Types...>::operator=...;
+
+        Variant& operator=(Variant&& source);
+        Variant& operator=(const Variant& source);
+
+        template<typename... SourceTypes>
+        Variant& operator=(Variant<SourceTypes...>&& source);
+        template<typename... SourceTypes>
+        Variant& operator=(const Variant<SourceTypes...>& source);
+
+        template <typename T>
+        bool is() const noexcept;          
+        
+        template <typename T> 
+        T& get() & noexcept;               
+
+        template <typename T> 
+        const T& get() const & noexcept;      
+        
+        template <typename T> 
+        T&& get() && noexcept
+
+        template <typename R = ComputedResultType, typename Visitor>
+        VisitResult<R, Visitor, Types&...> 
+        visit(Visitor&& v) & ;
+        
+        template <typename R = ComputedResultType, typename Visitor>
+        VisitResult<R, Visitor, const Types&...> 
+        visit(Visitor&& v) const & ;
+        
+        template<typename R = ComputedResultType, typename Visitor>
+        VisitResult<R, Visitor, Types&&...> 
+        visit(Visitor&& v) && ;
+
+        bool isEmpty() const noexcept;
+
+    private:
+        void destroy();              
+    };
+}
